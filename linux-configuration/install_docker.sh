b672b80dcba37d515da04b8c3476811325ea8a9f
JinHuang <54jin.huang@gmail.com>
2019-10-24 14:43:47 +0800

    docker-ce install script

---
+++ linux-configuration/install_docker.sh
@@ -0,0 +1,10 @@
+apt-get remove docker \
+docker-engine \
+docker.io
+
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+"deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
