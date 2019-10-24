1ac9f2d4584b9855840f4162cf0481d1445e908f
JinHuang <54jin.huang@gmail.com>
2019-10-24 14:53:46 +0800

    add install command

--- linux-configuration/install_docker.sh
+++ linux-configuration/install_docker.sh
@@ -8,3 +8,7 @@ sudo add-apt-repository \
 "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
+
+sudo apt-get remove docker \
+docker-engine \
+docker.io
