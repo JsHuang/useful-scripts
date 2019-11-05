ee8a5bf37aa0e842c61cfc0d918b62fff4949210
JinHuang <54jin.huang@gmail.com>
2019-11-05 10:52:33 +0800

    add daemon.json and add to docker group

--- linux-configuration/install_docker.sh
+++ linux-configuration/install_docker.sh
@@ -2,15 +2,31 @@ apt-get remove docker \
 docker-engine \
 docker.io
 
-curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | apt-key add -
 
-sudo add-apt-repository \
+add-apt-repository \
 "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
 
-sudo apt-get remove docker \
+apt-get remove docker \
 docker-engine \
 docker.io
 
-sudo apt-get install docker-ce
+apt-get install docker-ce
+
+cat>/etc/docker/daemon.json<<EOF
+{
+    "registry-mirrors": [
+        "http://af4d232c.m.daocloud.io"
+    ],
+    "insecure-registries": []
+}
+EOF
+
+service docker restart
+
+groupadd docker
+usermod -aG docker $USER
+
+docker run hello-world
